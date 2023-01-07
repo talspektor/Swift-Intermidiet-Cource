3ab1fd53d905cba59dd7292126f54fdb8fae9d60
tspektor <tal_spektor@intuit.com>
2023-01-07 12:22:15 +0200

    closures

---
+++ Functions/closures.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+let hello: (String) -> () = { name in
+    print("Hello \(name)!")
+}
+
+hello("Mary Doe")
+
+let pow: (Int, Int) -> Int = {
+    $0 * $1
+}
+
+let result = pow(5,3)
+print(result)
+
+
+func getPosts(completion: @escaping ([String]) -> ())  {
+    
+    var posts: [String] = []
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+        posts = ["Hello World", "Introduction to Closures"]
+        completion(posts)
+    }
+    
+}
+
+getPosts { (posts) in
+    print(posts)
+}
